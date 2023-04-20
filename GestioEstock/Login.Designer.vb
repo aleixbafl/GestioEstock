3ef3805265a511ea499f03366633744030c57098
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2023-04-21 00:07:22 +0200

    0.2_perzonalitzar_finestra

--- GestioEstock/Login.Designer.vb
+++ GestioEstock/Login.Designer.vb
@@ -22,17 +22,36 @@ Partial Class Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.tanca = New System.Windows.Forms.PictureBox()
+        CType(Me.tanca, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'tanca
+        '
+        Me.tanca.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tanca.Image = CType(resources.GetObject("tanca.Image"), System.Drawing.Image)
+        Me.tanca.Location = New System.Drawing.Point(758, 12)
+        Me.tanca.Name = "tanca"
+        Me.tanca.Size = New System.Drawing.Size(30, 30)
+        Me.tanca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.tanca.TabIndex = 0
+        Me.tanca.TabStop = False
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.tanca)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Login"
         Me.Text = "Login"
+        CType(Me.tanca, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents tanca As PictureBox
 End Class
--- GestioEstock/Login.vb
+++ GestioEstock/Login.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Login
 
+    Private Sub tanca_MouseEnter(sender As Object, e As EventArgs) Handles tanca.MouseEnter
+        Cursor = Cursors.Hand
+    End Sub
+    Private Sub tanca_MouseLeave(sender As Object, e As EventArgs) Handles tanca.MouseLeave
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub tanca_Click(sender As Object, e As EventArgs) Handles tanca.Click
+        Application.Exit()
+    End Sub
 End Class
