b1820f4645449c161e607131f357492332dd0914
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2023-04-20 23:51:55 +0200

    0.1.5_error

--- GestioEstock/My Project/Application.Designer.vb
+++ GestioEstock/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GestioEstock.Form1
+            Me.MainForm = Global.GestioEstock.Login
         End Sub
     End Class
 End Namespace
