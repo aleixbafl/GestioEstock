5c553321e7e7291f1e47024f534517acf4406566
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2023-04-21 00:23:49 +0200

    0.2.1_visual

--- GestioEstock/Login.Designer.vb
+++ GestioEstock/Login.Designer.vb
@@ -24,34 +24,50 @@ Partial Class Login
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
         Me.tanca = New System.Windows.Forms.PictureBox()
+        Me.minimitzar = New System.Windows.Forms.PictureBox()
         CType(Me.tanca, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'tanca
         '
         Me.tanca.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.tanca.Image = CType(resources.GetObject("tanca.Image"), System.Drawing.Image)
-        Me.tanca.Location = New System.Drawing.Point(758, 12)
+        Me.tanca.Location = New System.Drawing.Point(963, 12)
         Me.tanca.Name = "tanca"
-        Me.tanca.Size = New System.Drawing.Size(30, 30)
+        Me.tanca.Size = New System.Drawing.Size(25, 25)
         Me.tanca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.tanca.TabIndex = 0
         Me.tanca.TabStop = False
+        '
+        'minimitzar
+        '
+        Me.minimitzar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.minimitzar.Image = CType(resources.GetObject("minimitzar.Image"), System.Drawing.Image)
+        Me.minimitzar.Location = New System.Drawing.Point(922, 12)
+        Me.minimitzar.Name = "minimitzar"
+        Me.minimitzar.Size = New System.Drawing.Size(25, 25)
+        Me.minimitzar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.minimitzar.TabIndex = 1
+        Me.minimitzar.TabStop = False
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1000, 500)
+        Me.Controls.Add(Me.minimitzar)
         Me.Controls.Add(Me.tanca)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Login"
         Me.Text = "Login"
         CType(Me.tanca, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents tanca As PictureBox
+    Friend WithEvents minimitzar As PictureBox
 End Class
--- GestioEstock/Login.vb
+++ GestioEstock/Login.vb
@@ -1,13 +1,31 @@
 ﻿Public Class Login
-
-    Private Sub tanca_MouseEnter(sender As Object, e As EventArgs) Handles tanca.MouseEnter
+    Private Sub cursorPuntero(obj As Object)
         Cursor = Cursors.Hand
     End Sub
-    Private Sub tanca_MouseLeave(sender As Object, e As EventArgs) Handles tanca.MouseLeave
+    Private Sub cursorPrincimi(obj As Object)
         Cursor = Cursors.Default
+    End Sub
+
+    Private Sub tanca_MouseEnter(sender As Object, e As EventArgs) Handles tanca.MouseEnter
+        cursorPuntero(tanca)
+    End Sub
+
+    Private Sub tanca_MouseLeave(sender As Object, e As EventArgs) Handles tanca.MouseLeave
+        cursorPrincimi(tanca)
     End Sub
 
     Private Sub tanca_Click(sender As Object, e As EventArgs) Handles tanca.Click
         Application.Exit()
+    End Sub
+    Private Sub minimitzar_MouseEnter(sender As Object, e As EventArgs) Handles minimitzar.MouseEnter
+        cursorPuntero(tanca)
+    End Sub
+
+    Private Sub minimitzar_MouseLeave(sender As Object, e As EventArgs) Handles minimitzar.MouseLeave
+        cursorPrincimi(tanca)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles minimitzar.Click
+        Me.WindowState = FormWindowState.Minimized
     End Sub
 End Class
