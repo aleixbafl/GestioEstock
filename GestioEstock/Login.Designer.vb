956f643b3e6ca6600aa60440a6b78c450fbb2282
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2023-04-21 16:16:10 +0200

    cambis grafics

--- GestioEstock/Login.Designer.vb
+++ GestioEstock/Login.Designer.vb
@@ -40,7 +40,7 @@ Partial Class Login
         Me.tanca.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.tanca.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.tanca.Image = CType(resources.GetObject("tanca.Image"), System.Drawing.Image)
-        Me.tanca.Location = New System.Drawing.Point(722, 8)
+        Me.tanca.Location = New System.Drawing.Point(872, 8)
         Me.tanca.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tanca.Name = "tanca"
         Me.tanca.Size = New System.Drawing.Size(19, 20)
@@ -53,7 +53,7 @@ Partial Class Login
         Me.minimitzar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.minimitzar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.minimitzar.Image = CType(resources.GetObject("minimitzar.Image"), System.Drawing.Image)
-        Me.minimitzar.Location = New System.Drawing.Point(692, 8)
+        Me.minimitzar.Location = New System.Drawing.Point(842, 8)
         Me.minimitzar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.minimitzar.Name = "minimitzar"
         Me.minimitzar.Size = New System.Drawing.Size(19, 20)
@@ -65,7 +65,7 @@ Partial Class Login
         '
         Me.adaptarFinestra.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.adaptarFinestra.Image = CType(resources.GetObject("adaptarFinestra.Image"), System.Drawing.Image)
-        Me.adaptarFinestra.Location = New System.Drawing.Point(728, 382)
+        Me.adaptarFinestra.Location = New System.Drawing.Point(878, 476)
         Me.adaptarFinestra.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.adaptarFinestra.Name = "adaptarFinestra"
         Me.adaptarFinestra.Size = New System.Drawing.Size(19, 20)
@@ -81,7 +81,7 @@ Partial Class Login
         Me.barraSuperiorMoure.Location = New System.Drawing.Point(0, 0)
         Me.barraSuperiorMoure.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.barraSuperiorMoure.Name = "barraSuperiorMoure"
-        Me.barraSuperiorMoure.Size = New System.Drawing.Size(687, 32)
+        Me.barraSuperiorMoure.Size = New System.Drawing.Size(837, 32)
         Me.barraSuperiorMoure.TabIndex = 3
         Me.barraSuperiorMoure.TabStop = False
         '
@@ -89,7 +89,7 @@ Partial Class Login
         '
         Me.barraSuperiorPartBotons.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.barraSuperiorPartBotons.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.barraSuperiorPartBotons.Location = New System.Drawing.Point(686, 0)
+        Me.barraSuperiorPartBotons.Location = New System.Drawing.Point(836, 0)
         Me.barraSuperiorPartBotons.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.barraSuperiorPartBotons.Name = "barraSuperiorPartBotons"
         Me.barraSuperiorPartBotons.Size = New System.Drawing.Size(64, 32)
@@ -101,7 +101,7 @@ Partial Class Login
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(750, 406)
+        Me.ClientSize = New System.Drawing.Size(900, 500)
         Me.Controls.Add(Me.barraSuperiorPartBotons)
         Me.Controls.Add(Me.barraSuperiorMoure)
         Me.Controls.Add(Me.adaptarFinestra)
