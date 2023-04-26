c6c87102dba82297623be492dfe54617e94682cd
AlexAiguadeAlisultanov <129412789+AlexAiguadeAlisultanov@users.noreply.github.com>
2023-04-26 15:47:27 +0200

    logo+ boton

--- GestioEstock/Login.Designer.vb
+++ GestioEstock/Login.Designer.vb
@@ -23,79 +23,20 @@ Partial Class MenuPrinEmpleat
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MenuPrinEmpleat))
-        Me.tanca = New System.Windows.Forms.PictureBox()
-        Me.minimitzar = New System.Windows.Forms.PictureBox()
-        Me.barraSuperiorMoure = New System.Windows.Forms.PictureBox()
-        Me.barraSuperiorPartBotons = New System.Windows.Forms.PictureBox()
-        Me.adaptarFinestra = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        CType(Me.tanca, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.barraSuperiorMoure, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.barraSuperiorPartBotons = New System.Windows.Forms.PictureBox()
+        Me.barraSuperiorMoure = New System.Windows.Forms.PictureBox()
+        Me.adaptarFinestra = New System.Windows.Forms.PictureBox()
+        Me.minimitzar = New System.Windows.Forms.PictureBox()
+        Me.tanca = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.barraSuperiorPartBotons, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.barraSuperiorMoure, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.adaptarFinestra, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tanca, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'tanca
-        '
-        Me.tanca.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.tanca.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.tanca.Image = CType(resources.GetObject("tanca.Image"), System.Drawing.Image)
-        Me.tanca.Location = New System.Drawing.Point(872, 8)
-        Me.tanca.Margin = New System.Windows.Forms.Padding(2)
-        Me.tanca.Name = "tanca"
-        Me.tanca.Size = New System.Drawing.Size(19, 20)
-        Me.tanca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.tanca.TabIndex = 0
-        Me.tanca.TabStop = False
-        '
-        'minimitzar
-        '
-        Me.minimitzar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.minimitzar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.minimitzar.Image = CType(resources.GetObject("minimitzar.Image"), System.Drawing.Image)
-        Me.minimitzar.Location = New System.Drawing.Point(842, 8)
-        Me.minimitzar.Margin = New System.Windows.Forms.Padding(2)
-        Me.minimitzar.Name = "minimitzar"
-        Me.minimitzar.Size = New System.Drawing.Size(19, 20)
-        Me.minimitzar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.minimitzar.TabIndex = 1
-        Me.minimitzar.TabStop = False
-        '
-        'barraSuperiorMoure
-        '
-        Me.barraSuperiorMoure.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.barraSuperiorMoure.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.barraSuperiorMoure.Location = New System.Drawing.Point(0, 0)
-        Me.barraSuperiorMoure.Margin = New System.Windows.Forms.Padding(2)
-        Me.barraSuperiorMoure.Name = "barraSuperiorMoure"
-        Me.barraSuperiorMoure.Size = New System.Drawing.Size(837, 32)
-        Me.barraSuperiorMoure.TabIndex = 3
-        Me.barraSuperiorMoure.TabStop = False
-        '
-        'barraSuperiorPartBotons
-        '
-        Me.barraSuperiorPartBotons.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.barraSuperiorPartBotons.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.barraSuperiorPartBotons.Location = New System.Drawing.Point(836, 0)
-        Me.barraSuperiorPartBotons.Margin = New System.Windows.Forms.Padding(2)
-        Me.barraSuperiorPartBotons.Name = "barraSuperiorPartBotons"
-        Me.barraSuperiorPartBotons.Size = New System.Drawing.Size(64, 32)
-        Me.barraSuperiorPartBotons.TabIndex = 4
-        Me.barraSuperiorPartBotons.TabStop = False
-        '
-        'adaptarFinestra
-        '
-        Me.adaptarFinestra.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.adaptarFinestra.Image = CType(resources.GetObject("adaptarFinestra.Image"), System.Drawing.Image)
-        Me.adaptarFinestra.Location = New System.Drawing.Point(878, 476)
-        Me.adaptarFinestra.Margin = New System.Windows.Forms.Padding(2)
-        Me.adaptarFinestra.Name = "adaptarFinestra"
-        Me.adaptarFinestra.Size = New System.Drawing.Size(19, 20)
-        Me.adaptarFinestra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.adaptarFinestra.TabIndex = 2
-        Me.adaptarFinestra.TabStop = False
         '
         'Button1
         '
@@ -111,6 +52,79 @@ Partial Class MenuPrinEmpleat
         Me.Button1.Text = "Login"
         Me.Button1.UseVisualStyleBackColor = False
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.GestioEstock.My.Resources.Resources.Logo_PcComponentes1
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(42, 43)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'barraSuperiorPartBotons
+        '
+        Me.barraSuperiorPartBotons.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.barraSuperiorPartBotons.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.barraSuperiorPartBotons.Location = New System.Drawing.Point(836, 0)
+        Me.barraSuperiorPartBotons.Margin = New System.Windows.Forms.Padding(2)
+        Me.barraSuperiorPartBotons.Name = "barraSuperiorPartBotons"
+        Me.barraSuperiorPartBotons.Size = New System.Drawing.Size(64, 43)
+        Me.barraSuperiorPartBotons.TabIndex = 4
+        Me.barraSuperiorPartBotons.TabStop = False
+        '
+        'barraSuperiorMoure
+        '
+        Me.barraSuperiorMoure.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.barraSuperiorMoure.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.barraSuperiorMoure.Location = New System.Drawing.Point(0, 0)
+        Me.barraSuperiorMoure.Margin = New System.Windows.Forms.Padding(2)
+        Me.barraSuperiorMoure.Name = "barraSuperiorMoure"
+        Me.barraSuperiorMoure.Size = New System.Drawing.Size(837, 43)
+        Me.barraSuperiorMoure.TabIndex = 3
+        Me.barraSuperiorMoure.TabStop = False
+        '
+        'adaptarFinestra
+        '
+        Me.adaptarFinestra.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.adaptarFinestra.Image = CType(resources.GetObject("adaptarFinestra.Image"), System.Drawing.Image)
+        Me.adaptarFinestra.Location = New System.Drawing.Point(878, 476)
+        Me.adaptarFinestra.Margin = New System.Windows.Forms.Padding(2)
+        Me.adaptarFinestra.Name = "adaptarFinestra"
+        Me.adaptarFinestra.Size = New System.Drawing.Size(19, 20)
+        Me.adaptarFinestra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.adaptarFinestra.TabIndex = 2
+        Me.adaptarFinestra.TabStop = False
+        '
+        'minimitzar
+        '
+        Me.minimitzar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.minimitzar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.minimitzar.Image = CType(resources.GetObject("minimitzar.Image"), System.Drawing.Image)
+        Me.minimitzar.Location = New System.Drawing.Point(842, 8)
+        Me.minimitzar.Margin = New System.Windows.Forms.Padding(2)
+        Me.minimitzar.Name = "minimitzar"
+        Me.minimitzar.Size = New System.Drawing.Size(19, 20)
+        Me.minimitzar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.minimitzar.TabIndex = 1
+        Me.minimitzar.TabStop = False
+        '
+        'tanca
+        '
+        Me.tanca.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tanca.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.tanca.Image = CType(resources.GetObject("tanca.Image"), System.Drawing.Image)
+        Me.tanca.Location = New System.Drawing.Point(872, 8)
+        Me.tanca.Margin = New System.Windows.Forms.Padding(2)
+        Me.tanca.Name = "tanca"
+        Me.tanca.Size = New System.Drawing.Size(19, 20)
+        Me.tanca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.tanca.TabIndex = 0
+        Me.tanca.TabStop = False
+        '
         'MenuPrinEmpleat
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -118,6 +132,7 @@ Partial Class MenuPrinEmpleat
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(900, 500)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.barraSuperiorPartBotons)
         Me.Controls.Add(Me.barraSuperiorMoure)
         Me.Controls.Add(Me.adaptarFinestra)
@@ -127,11 +142,12 @@ Partial Class MenuPrinEmpleat
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "MenuPrinEmpleat"
         Me.Text = "Login"
-        CType(Me.tanca, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.barraSuperiorMoure, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.barraSuperiorPartBotons, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.barraSuperiorMoure, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.adaptarFinestra, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.minimitzar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tanca, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -142,4 +158,5 @@ Partial Class MenuPrinEmpleat
     Friend WithEvents barraSuperiorPartBotons As PictureBox
     Friend WithEvents adaptarFinestra As PictureBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
